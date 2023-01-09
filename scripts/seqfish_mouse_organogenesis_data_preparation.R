693dad7e34040554ec7ddfc07e6485d94e14d361
sebastianbirk <sebastian.birk@outlook.com>
2023-01-09 23:52:08 +0100

    Readd seqfish mouse organogenesis with imputed data

---
+++ scripts/seqfish_mouse_organogenesis_data_preparation.R
@@ -0,0 +1,61 @@
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install(version = "3.16")
+BiocManager::install("zellkonverter")
+BiocManager::install("scRNAseq")
+<<<<<<< HEAD
+BiocManager::install("rhdf5")
+=======
+>>>>>>> 6619c23b9bd35a18507787553e227cb6a7367cd2
+
+library(data.table)
+library(SingleCellExperiment)
+library(zellkonverter)
+<<<<<<< HEAD
+library(rhdf5)
+
+metadata <- readRDS(url("https://content.cruk.cam.ac.uk/jmlab/SpatialMouseAtlas2020/metadata.Rds","rb"))
+
+########################
+##### seqFISH data #####
+########################
+
+counts <- readRDS(url("https://content.cruk.cam.ac.uk/jmlab/SpatialMouseAtlas2020/counts.Rds","rb"))
+sce <- SingleCellExperiment(
+  list(counts=counts),
+  colData=DataFrame(Area=metadata$Area,
+                    celltype_mapped_refined=metadata$celltype_mapped_refined,
+                    x=metadata$x_global_affine,
+                    y=metadata$y_global_affine))
+writeH5AD(sce, file = "seqfish_mouse_organogenesis.h5ad")
+
+########################
+##### Imputed data #####
+########################
+
+download.file("https://content.cruk.cam.ac.uk/jmlab/SpatialMouseAtlas2020/imputed.h5", "imputed.h5")
+imputed_row_names <- readRDS(url("https://content.cruk.cam.ac.uk/jmlab/SpatialMouseAtlas2020/imputed_row_names.Rds", "rb"))
+imputed_col_names <- readRDS(url("https://content.cruk.cam.ac.uk/jmlab/SpatialMouseAtlas2020/imputed_column_names.Rds", "rb"))
+expr_imputed <- as(h5read("imputed.h5", "/logcounts"), "dgCMatrix")
+sce <- SingleCellExperiment(
+  list(counts=expr_imputed),
+  colData=DataFrame(Area=metadata$Area,
+                    celltype_mapped_refined=metadata$celltype_mapped_refined,
+                    x=metadata$x_global_affine,
+                    y=metadata$y_global_affine))ß
+rownames(sce) <- imputed_row_names
+colnames(sce) <- imputed_col_names
+# sce <- sce[, grepl("embryo2", colnames(sce))]
+writeH5AD(sce, file = "seqfish_mouse_organogenesis_imputed.h5ad")
+=======
+
+counts <- readRDS(url("https://content.cruk.cam.ac.uk/jmlab/SpatialMouseAtlas2020/counts.Rds","rb"))
+metadata <- readRDS(url("https://content.cruk.cam.ac.uk/jmlab/SpatialMouseAtlas2020/metadata.Rds","rb"))
+
+sce <- SingleCellExperiment(
+  list(counts=counts),
+  colData=DataFrame(Area=metadata$Area,
+                    celltype_mapped_refined=metadata$celltype_mapped_refined))
+
+writeH5AD(sce, file = "seqfish_mouse_organogenesis.h5ad")
+>>>>>>> 6619c23b9bd35a18507787553e227cb6a7367cd2
