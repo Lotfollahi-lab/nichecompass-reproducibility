ad1d91ad58d18d097e124cf2352239004a9aac64
chelseabright96 <chels.alex.bright@gmail.com>
2023-03-23 18:01:38 +0100

    Work on batch integration evaluation

---
+++ scripts/train_autotalker_reference_model_test.sh
@@ -0,0 +1,17 @@
+python train_autotalker_reference_model.py \
+--dataset starmap_plus_mouse_cns \
+--reference_batches batch1 batch2 batch3 \
+--n_neighbors 4 \
+--filter_genes \
+--n_hvg 2000 \
+--nichenet_max_n_target_genes_per_gp 20000 \
+--include_mebocost_gps \
+--mebocost_species mouse \
+--counts_key counts \
+--log_variational \
+--n_epochs 1 \
+--n_epochs_all_gps 1 \
+--lambda_group_lasso 0. \
+--lambda_l1_masked 0. \
+--edge_batch_size 128 \
+--node_batch_size 16
