632ac80db55e4d5b06812d8bd168504b0840df63
Sebastian Birk <sebastian.birk@outlook.com>
2023-05-22 03:41:33 -0400

    Add one hop attention script for nanostring cosmx human nsclc

--- scripts/reference/train_nanostring_cosmx_human_nsclc_nichecompass_one-hop-attention_reference_model.sh
+++ scripts/reference/train_nanostring_cosmx_human_nsclc_nichecompass_one-hop-attention_reference_model.sh
@@ -21,14 +21,14 @@ python ../train_nichecompass_reference_model.py \
 --gp_targets_mask_key nichecompass_gp_targets \
 --gp_sources_mask_key nichecompass_gp_sources \
 --gp_names_key nichecompass_gp_names \
---model_label one-hop-norm_reference \
+--model_label one-hop-attention_reference \
 --active_gp_names_key nichecompass_active_gp_names \
 --latent_key nichecompass_latent \
 --active_gp_thresh_ratio 0.1 \
 --gene_expr_recon_dist nb \
 --cond_embed_injection gene_expr_decoder \
 --no-log_variational \
---node_label_method one-hop-norm \
+--node_label_method one-hop-attention \
 --n_layers_encoder 1 \
 --conv_layer_encoder gcnconv \
 --n_epochs 100 \
@@ -40,5 +40,5 @@ python ../train_nichecompass_reference_model.py \
 --contrastive_logits_ratio 0. \
 --lambda_group_lasso 0. \
 --lambda_l1_masked 30. \
---edge_batch_size 1024 \
+--edge_batch_size 512 \
 --node_batch_size None
