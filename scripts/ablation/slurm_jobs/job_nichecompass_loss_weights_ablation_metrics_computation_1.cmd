07ed04b95da4d3bda728798da2a055e41111bd39
sebastian.birk <sebastian.birk@outlook.com>
2023-07-13 09:11:17 +0200

    Add newest ablation runs

--- scripts/ablation/slurm_jobs/job_nichecompass_loss_weights_ablation_metrics_computation_1.cmd
+++ scripts/ablation/slurm_jobs/job_nichecompass_loss_weights_ablation_metrics_computation_1.cmd
@@ -13,4 +13,4 @@ source $HOME/.bashrc
 conda activate nichecompass-test
 cd /
 cd /home/aih/sebastian.birk/workspace/projects/nichecompass-reproducibility/scripts/ablation
-python ../compute_metrics.py --task loss_weights_ablation --file_name mlflow_summary_loss_weights_ablation_60.csv --datasets xenium_human_breast_cancer --cell_type_keys cell_states --batch_keys None
+python ../compute_metrics.py --task loss_weights_ablation --file_name mlflow_summary_loss_weights_ablation_160.csv --datasets xenium_human_breast_cancer --cell_type_keys cell_states --batch_keys None
