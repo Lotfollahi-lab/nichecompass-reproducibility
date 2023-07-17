f8be9a3e07bd449a0ec5c52ece09bc9f94c55015
sebastian.birk <sebastian.birk@outlook.com>
2023-07-17 18:00:36 +0200

    Add new ablation logs

---
+++ scripts/ablation/slurm_jobs/job_nichecompass_loss_weights_ablation_xenium_human_breast_cancer_metrics_computation_3.cmd
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -J nichecompass_loss_weights_ablation_xenium_human_breast_cancer_metrics_computation_3
+#SBATCH -o ../scripts/ablation/slurm_jobs/logs/out_nichecompass_loss_weights_ablation_xenium_human_breast_cancer_metrics_computation_3.txt
+#SBATCH -e ../scripts/ablation/slurm_jobs/logs/err_nichecompass_loss_weights_ablation_xenium_human_breast_cancer_metrics_computation_3.txt
+#SBATCH -t 48:00:00
+#SBATCH -p gpu_p
+#SBATCH -c 6
+#SBATCH --gres=gpu:1
+#SBATCH --qos=gpu
+#SBATCH --mem=64GB
+#SBATCH --nice=10000
+source $HOME/.bashrc
+conda activate nichecompass-test
+cd /
+cd /home/aih/sebastian.birk/workspace/projects/nichecompass-reproducibility/scripts/ablation
+python ../compute_metrics.py --task loss_weights_ablation --file_name mlflow_summary_loss_weights_ablation_xenium_human_breast_cancer_80.csv --datasets xenium_human_breast_cancer --cell_type_keys cell_states --batch_keys None --metrics gcs nasw cnmi
