bab1afe0f5551b8e719e09cb9aa6261df6034d14
Sebastian Birk <sebastian.birk@outlook.com>
2023-08-15 09:10:45 -0400

    Add changes to notebooks and utils from Carlos GPU server

--- utils/benchmarking_utils.py
+++ utils/benchmarking_utils.py
@@ -1,9 +1,12 @@
+import os
+import pickle
+
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import scanpy as sc
-import scib
 
-from nichecompass.benchmarking import compute_clisis, compute_cas
+from nichecompass.benchmarking import compute_benchmarking_metrics, compute_clisis, compute_cas
 
 
 def compute_latent_space_comparison(dataset,
@@ -247,6 +250,266 @@ def plot_batch_integration_results(df,
             for j, col in enumerate(aggregate_cols)]
         
     print(column_definitions)
+        
+    # Allow to manipulate text post-hoc (in illustrator)
+    with matplotlib.rc_context({"svg.fonttype": "none"}):
+        fig, ax = plt.subplots(figsize=(len(df.columns) * 1., 3 + 0.3 * len(df.columns)))
+        tab = Table(
+            df,
+            cell_kw={
+                "linewidth": 0,
+                "edgecolor": "k"},
+            column_definitions=column_definitions,
+            ax=ax,
+            row_dividers=True,
+            footer_divider=True,
+            textprops={"fontsize": 10, "ha": "center"},
+            row_divider_kw={"linewidth": 1, "linestyle": (0, (1, 5))},
+            col_label_divider_kw={"linewidth": 1, "linestyle": "-"},
+            column_border_kw={"linewidth": 1, "linestyle": "-"},
+            index_col="Model",
+        ).autoset_fontcolors(colnames=df.columns)
+    if show:
+        plt.show()
+    if save_dir is not None:
+        os.makedirs(save_dir, exist_ok=True)        
+        fig.savefig(os.path.join(save_dir, save_name), facecolor=ax.get_facecolor(), dpi=300)
+    return tab
+
+
+def compute_combined_benchmarking_metrics(model_adata,
+                                          model_name,
+                                          metrics,
+                                          cell_type_key="cell_type",
+                                          spatial_key="spatial",
+                                          run_number_list=list(np.arange(1, 9)),
+                                          n_neighbors_list=[4, 4, 8, 8, 12, 12, 16, 16]):
+    benchmarking_dict_list = []
+    for run_number, n_neighbors in zip(run_number_list, n_neighbors_list):
+        
+        print(model_adata)
+        
+        # Compute NicheCompass benchmarking metrics
+        benchmarking_dict = compute_benchmarking_metrics(adata=model_adata,
+                                                         metrics=metrics,
+                                                         cell_type_key=cell_type_key,
+                                                         spatial_key=spatial_key,
+                                                         latent_key=f"{model_name}_latent_run{run_number}")
+        benchmarking_dict["model_name"] = model_name
+        benchmarking_dict["run"] = run_number
+        benchmarking_dict_list.append(benchmarking_dict)
+    return benchmarking_dict_list
+
+
+def compute_combined_benchmarking_metrics_for_all_models(
+        dataset,
+        metrics=["gcs",
+                 "mlami",
+                 "cas",
+                 "clisis",
+                 "cnmi",
+                 "casw",
+                 "clisi",
+                 "nasw",
+                 "basw",
+                 "bgc",
+                 "blisi",
+                 "kbet"],
+        cell_type_key="cell_type",
+        run_number_list=list(np.arange(1, 9)),
+        n_neighbors_list=[4, 4, 8, 8, 12, 12, 16, 16],
+        included_models=["nichecompass",
+                         "deeplinc",
+                         "graphst",
+                         "sagenet",
+                         "expimap",
+                         "scvi"],
+        benchmarking_type="single_sample_method_benchmarking"):
+    # Configure dataset artifact folder path
+    benchmarking_folder_path = f"../../artifacts/{benchmarking_type}/"
+    os.makedirs(benchmarking_folder_path, exist_ok=True)
+    
+    benchmarking_dict_list = []
+    
+    # scVI
+    if "scvi" in included_models:
+        print("Computing metrics for scVI...")
+        adata_scvi = sc.read_h5ad(benchmarking_folder_path + f"{dataset}_scvi.h5ad")
+        benchmarking_dict_list_scvi = compute_combined_benchmarking_metrics(
+            model_adata=adata_scvi,
+            model_name="scvi",
+            metrics=metrics,
+            run_number_list=run_number_list,
+            n_neighbors_list=n_neighbors_list,
+            cell_type_key=cell_type_key)  
+        benchmarking_dict_list += benchmarking_dict_list_scvi
+        with open(f"{benchmarking_folder_path}/benchmarking_dict_list.pickle", "wb") as f:
+            pickle.dump(benchmarking_dict_list, f)
+        del(adata_scvi)
+        print("")
+
+    # expiMap
+    if "expimap" in included_models:
+        print("Computing metrics for expiMap...")
+        adata_expimap = sc.read_h5ad(benchmarking_folder_path + f"{dataset}_expimap.h5ad")
+        benchmarking_dict_list_expimap = compute_combined_benchmarking_metrics(
+            model_adata=adata_expimap,
+            model_name="expimap",
+            metrics=metrics,
+            run_number_list=run_number_list,
+            n_neighbors_list=n_neighbors_list,
+            cell_type_key=cell_type_key)  
+        benchmarking_dict_list += benchmarking_dict_list_expimap
+        with open(f"{benchmarking_folder_path}/benchmarking_dict_list.pickle", "wb") as f:
+            pickle.dump(benchmarking_dict_list, f)
+        del(adata_expimap)
+        print("")
+    
+    # SageNet
+    if "sagenet" in included_models:
+        print("Computing metrics for SageNet...")
+        adata_sagenet = sc.read_h5ad(benchmarking_folder_path + f"{dataset}_sagenet.h5ad")
+        benchmarking_dict_list_sagenet = compute_combined_benchmarking_metrics(
+            model_adata=adata_sagenet,
+            model_name="sagenet",
+            metrics=metrics,
+            run_number_list=run_number_list,
+            n_neighbors_list=n_neighbors_list,
+            cell_type_key=cell_type_key) 
+        benchmarking_dict_list += benchmarking_dict_list_sagenet
+        with open(f"{benchmarking_folder_path}/benchmarking_dict_list.pickle", "wb") as f:
+            pickle.dump(benchmarking_dict_list, f)
+        del(adata_sagenet)
+        print("")
+    
+    # DeepLinc
+    if "deeplinc" in included_models:
+        print("Computing metrics for DeepLinc...")
+        adata_deeplinc = sc.read_h5ad(benchmarking_folder_path + f"{dataset}_deeplinc.h5ad")
+        benchmarking_dict_list_deeplinc = compute_combined_benchmarking_metrics(
+            model_adata=adata_deeplinc,
+            model_name="deeplinc",
+            metrics=metrics,
+            run_number_list=run_number_list,
+            n_neighbors_list=n_neighbors_list,
+            cell_type_key=cell_type_key)
+        benchmarking_dict_list += benchmarking_dict_list_deeplinc
+        with open(f"{benchmarking_folder_path}/benchmarking_dict_list.pickle", "wb") as f:
+            pickle.dump(benchmarking_dict_list, f)
+        del(adata_deeplinc)
+        print("")
+    
+    # GraphST
+    if "graphst" in included_models:
+        print("Computing metrics for GraphST...")
+        adata_graphst = sc.read_h5ad(benchmarking_folder_path + f"{dataset}_graphst.h5ad")
+        benchmarking_dict_list_graphst = compute_combined_benchmarking_metrics(
+            model_adata=adata_graphst,
+            model_name="graphst",
+            metrics=metrics,
+            run_number_list=run_number_list,
+            n_neighbors_list=n_neighbors_list,
+            cell_type_key=cell_type_key)
+        benchmarking_dict_list += benchmarking_dict_list_graphst
+        with open(f"{benchmarking_folder_path}/benchmarking_dict_list.pickle", "wb") as f:
+            pickle.dump(benchmarking_dict_list, f)
+        del(adata_graphst)
+        print("")
+
+    # NicheCompass
+    if "nichecompass" in included_models:
+        print("Computing metrics for NicheCompass...")
+        adata_nichecompass = sc.read_h5ad(benchmarking_folder_path + f"{dataset}_nichecompass.h5ad")
+        benchmarking_dict_list_nichecompass = compute_combined_benchmarking_metrics(
+            model_adata=adata_nichecompass,
+            model_name="nichecompass",
+            metrics=metrics,
+            run_number_list=run_number_list,
+            n_neighbors_list=n_neighbors_list,
+            cell_type_key=cell_type_key)
+        benchmarking_dict_list += benchmarking_dict_list_nichecompass
+        with open(f"{benchmarking_folder_path}/benchmarking_dict_list.pickle", "wb") as f:
+            pickle.dump(benchmarking_dict_list, f)
+        del(adata_nichecompass)
+        print("")
+        
+
+def truncate_colormap(cmap, minval=0.0, maxval=1.0, n=100):
+    new_cmap = colors.LinearSegmentedColormap.from_list(
+        'trunc({n},{a:.2f},{b:.2f})'.format(n=cmap.name, a=minval, b=maxval),
+        cmap(np.linspace(minval, maxval, n)))
+    return new_cmap
+
+def plot_benchmarking_results(df,
+                              show=True,
+                              save_dir=None,
+                              save_name="benchmarking_results.svg"):
+    datasets = df["dataset"].unique().tolist()
+    df = df.pivot(index=["model_name", "spatially_aware"], columns=["dataset", "score_type"], values="score")
+    df.reset_index(inplace=True)
+    df.columns = ['_'.join(col).strip("_") for col in df.columns.values]
+    if len(datasets) > 1:
+        for i, dataset in enumerate(datasets):
+            df[f"Total ({i})"] = df[[col for col in list(df.columns) if dataset in col]].mean(axis=1)
+        df["Total (All)"] = df[list(set(list(df.columns)) - set(["model_name", "spatially_aware"]))].mean(axis=1)
+        df.sort_values(by=["Total (All)"], inplace=True, ascending=False)
+    else:
+        df["Total"] = df[list(set(list(df.columns)) - set(["model_name"]))].mean(axis=1)
+        df.sort_values(by=["Total"], inplace=True, ascending=False)
+    df.rename(columns={"model_name": "Model"}, inplace=True)
+    
+    cmap_fn = lambda col_data: normed_cmap(col_data, cmap=matplotlib.cm.PRGn, num_stds=2.5)
+
+    column_definitions = [
+        ColumnDefinition(name="Model",
+                         title="Model",
+                         width=1.5,
+                         textprops={"ha": "left", "weight": "bold"}),
+        ColumnDefinition(name="spatially_aware",
+                         title="Spatially \n Aware",
+                         width=1.,
+                         formatter=tickcross)]
+
+    metric_cols = ["cas", "clisis", "gcs", "mlami"]
+    aggregate_cols = [col for col in list(df.columns) if "Total" in col]
+
+    for i, dataset in enumerate(datasets):
+        if len(datasets) > 1:
+            dataset_number_string = f"({i})"
+        else:
+            dataset_number_string = ""
+        # Circles for the metric columns
+        column_definitions += [
+            ColumnDefinition(
+                name=f"{dataset}_{col}",
+                title=col.upper(),
+                width=1,
+                textprops={
+                    "ha": "center",
+                    "bbox": {"boxstyle": "circle", "pad": 0.25}},
+                cmap=cmap_fn(df[f"{dataset}_{col}"]),
+                group=f"Metrics \n {dataset.replace('_', ' ').title()} {dataset_number_string}",
+                border="left" if j == 0 else None,
+                formatter="{:.2f}")
+            for j, col in enumerate(metric_cols)]
+
+        # Bars for the aggregate columns
+        column_definitions += [
+            ColumnDefinition(
+                name=col,
+                title=col,
+                width=1,
+                plot_fn=bar,
+                plot_kw={
+                    "cmap": truncate_colormap(matplotlib.cm.YlOrRd, 0, 0.8),
+                    "plot_bg_bar": False,
+                    "annotate": True,
+                    "height": 0.9,
+                    "formatter": "{:.2f}",
+                },
+                group="Aggregate Scores",
+                border="left" if j == 0 else None)
+            for j, col in enumerate(aggregate_cols)]
         
     # Allow to manipulate text post-hoc (in illustrator)
     with matplotlib.rc_context({"svg.fonttype": "none"}):
