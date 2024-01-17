2f9bbd032d0c7386adefa84183a828482fc2b088
sebastian.birk <sebastian.birk@outlook.com>
2024-01-17 23:01:08 +0100

    Update data preparation notebook and remove duplicate xenium notebooks

--- scripts/data_preparation/spatial_atac_rna_seq_mouse_embryo_and_brain_data_preparation.R
+++ scripts/data_preparation/spatial_atac_rna_seq_mouse_embryo_and_brain_data_preparation.R
@@ -12,18 +12,18 @@ library(Seurat)
 library(SeuratData)
 library(SeuratDisk)
 
-#####################
+############################
 ##### Mouse Embryo E13 #####
-#####################
+############################
 
 mouse_e13 <- readRDS(file = "datasets/srt_data/bronze/spatial_atac_rna_seq_mouse_embryo/E13_spatial_RNA_ATAC.rds")
 
 SaveH5Seurat(mouse_e13, filename = "datasets/srt_data/silver/spatial_atac_rna_seq_mouse_embryo_e13.h5Seurat")
 Convert("datasets/srt_data/silver/spatial_atac_rna_seq_mouse_embryo_e13.h5Seurat", dest = "h5ad")
 
-#####################
+###########################
 ##### Mouse Brain P22 #####
-#####################
+###########################
 
 mouse_p22 <- readRDS(file = "datasets/srt_data/bronze/spatial_atac_rna_seq_mouse_brain/P22mousebrain_spatial_RNA_ATAC.rds")
 
