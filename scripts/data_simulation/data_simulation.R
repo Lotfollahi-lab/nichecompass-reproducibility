1ad667bd331a2720f299d2a877873eb9d6576b21
sebastian.birk <sebastian.birk@outlook.com>
2024-06-17 23:28:08 +0200

    Finalize first version of simulation

--- scripts/data_simulation/data_simulation.R
+++ scripts/data_simulation/data_simulation.R
@@ -27,11 +27,10 @@ BiocManager::install("zellkonverter") # on MacOS, use gcc compiler
 # nano ~/.R/Makevars
 # CC = /usr/local/bin/gcc-10
 # CXX = /usr/local/bin/g++-10
-
 install.packages("readr")
 
 ##========================
-## Installation (MacOS)
+## Imports
 ##========================
 rm(list=ls())
 library(dplyr)
@@ -40,97 +39,9 @@ library(SingleCellExperiment)
 library(zellkonverter)
 library(readr)
 
-isid = 1
-foldchange = 3 # foldchange, 1, 3, 5, 10
-set.seed(isid)
-
-load(paste0("./starmap_1020_0410_seurat_filter_layer.rds"))
-info2   <- info %>% select(c(x,y,label)) %>% 
-				filter(label!="Smc") %>% as.data.frame()
-
-region_celltype_df <- matrix(0.1,4,4)
-diag(region_celltype_df) <- 0.7
-rownames(region_celltype_df) <- paste0("Region",1:4)
-colnames(region_celltype_df) <- paste0("Celltype",1:4)
-
-nGene = 2000
-nLoc = 5000
-
 ##========================
-## simulate spatial spot
+## Overwrite simulation function to use ligand-receptor-target GP data as input
 ##========================
-simLoc <- simNewLocs(newN= nLoc,lay_out="random",preLoc = info2)
-
-## assign the region labels
-simLoc %<>% mutate(region_label = case_when(
-		x <= 0.5*median(x) ~ "Region1",
-		x > 0.5*median(x) & x <= median(x)~ "Region2",
-		x > median(x) & x <= 1.5*median(x)~ "Region3",
-		TRUE ~ "Region4"
-	)) %>% as.data.frame()
-
-
-## load the Ligand-Receptor data
-load("./LR_dat.RData")
-
-##========================
-## Data Generation
-##========================
-
-## reference free
-example_CCI_free = srtsim_cci_free(
-					zero_prop_in = 0,
-					disper_in = Inf,
-					mu_in = 1, 
-					numGene = nGene,
-					location_in  = simLoc[,c("x","y","region_label")],
-					# region_label = region_label,
-					region_cell_map = region_celltype_df,
-					sim_seed = isid,
-					fc = foldchange,
-					LR_in = LR_dat)
-
-## reference based
-simSRT  <- createSRT(count_in=sp_count[,rownames(info2)],loc_in =info2)
-simSRT1 <- srtsim_fit(simSRT,sim_schem="domain")
-
-example_CCI_ref = srtsim_cci_ref(
-					EstParam = simSRT1@EstParam,
-					numGene = nGene,
-					location_in  = simLoc[,c("x","y","region_label")],
-					# region_label = region_label,
-					region_cell_map = region_celltype_df,
-					sim_seed = isid,
-					fc = foldchange,
-					LR_in = LR_dat)
-
-sce_free <- SingleCellExperiment(
-  list(counts=example_CCI_free@simCounts),
-  colData=DataFrame(region_label=example_CCI_free@simcolData$region_label,
-                    cell_type=example_CCI_free@simcolData$celltype,
-                    x=example_CCI_free@simcolData$x,
-                    y=example_CCI_free@simcolData$y))
-
-rownames(sce_free) <- example_CCI_free@simCounts@Dimnames[[1]]
-colnames(sce_free) <- example_CCI_free@simCounts@Dimnames[[2]]
-metadata(sce_free)$lr_data <- LR_dat
-writeH5AD(sce_free, file = "./simulated_free_1.h5ad")
-
-sce_ref <- SingleCellExperiment(
-  list(counts=example_CCI_ref@simCounts),
-  colData=DataFrame(region_label=example_CCI_ref@simcolData$region_label,
-                    cell_type=example_CCI_ref@simcolData$celltype,
-                    x=example_CCI_ref@simcolData$x,
-                    y=example_CCI_ref@simcolData$y))
-
-rownames(sce_ref) <- example_CCI_ref@simCounts@Dimnames[[1]]
-colnames(sce_ref) <- example_CCI_ref@simCounts@Dimnames[[2]]
-metadata(sce_ref)$lr_data <- LR_dat
-writeH5AD(sce_ref, file = "./simulated_ref_1.h5ad")
-
-original_function <- getAnywhere("srtsim_cci_ref")
-print(original_function)
-
 srtsim_cci_ref = function(
     EstParam = NULL,
     numGene = 1000,
@@ -207,36 +118,42 @@ srtsim_cci_ref = function(
   colnames(sim_cnt) <- paste0("Cell" ,1:numLoc)
   rownames(sim_cnt) <- paste0("Gene" ,1:numGene)
   
-  print(GP_in)
-  
   knn.list = FNN::get.knn(as.matrix(location_in[,c("x","y")]), k = numKNN)[[1]]
   gene_names = unique(unlist(GP_in[,1:2]))
-
-  print(gene_names)
-
+  
   simu_count = sim_cnt
   # gene_names = unique(unlist(LR_dat))
+  
+  # sample genes to keep from GP genes
+  if(dim(simu_count)[1] < length(gene_names)){
+    gene_names <- sample(gene_names, dim(simu_count)[1], replace = FALSE)
+  }
+  
   rownames(simu_count)[1:length(gene_names)] = gene_names
   # mat_foldchange_record = matrix(0, length(gene_names), dim(simu_count)[2])
   
   # assign cell types to regions
   print("Construct Communications...")
   for(celltype_A in unique(GP_in$celltypeA)){
-    print(celltype_A)
-    cellid_A = which(c_simu==celltype_A)
-    for(cellid in cellid_A){
-      nearest_cells = knn.list[cellid,]
+    for(region_A in unique(GP_in$regionA)){
+      cellid_A = which(c_simu==celltype_A & c_simu==region_A)
+      for(cellid in cellid_A){
+        nearest_cells = knn.list[cellid,]
         for(nearid in nearest_cells){
           nearest_celltypeB = c_simu[nearid]
           for(fc in unique(GP_in$fold_change)){
-              tmp_GP = GP_in[which(GP_in$celltypeA == celltype_A & GP_in$fold_change == fc & GP_in$celltypeB %in% nearest_celltypeB),]
-              source_gene_ind=na.omit(match(c(unlist(tmp_GP$sources)),rownames(simu_count)))
-              target_gene_ind=na.omit(match(c(unlist(tmp_GP$targets)),rownames(simu_count)))
-              if(length(source_gene_ind)>0)
-                simu_count[target_gene_ind,cellid] = fc*sim_cnt[target_gene_ind,cellid]
-              if(length(target_gene_ind)>0)
-                simu_count[source_gene_ind,nearid] = fc*sim_cnt[source_gene_ind,nearid]
+            tmp_GP = GP_in[which(GP_in$celltypeA == celltype_A &
+                                   GP_in$regionA == region_A &
+                                   GP_in$fold_change == fc &
+                                   GP_in$celltypeB %in% nearest_celltypeB),]
+            source_gene_ind=na.omit(match(c(unlist(tmp_GP$sources)),rownames(simu_count)))
+            target_gene_ind=na.omit(match(c(unlist(tmp_GP$targets)),rownames(simu_count)))
+            if(length(source_gene_ind)>0)
+              simu_count[target_gene_ind,cellid] = fc*sim_cnt[target_gene_ind,cellid]
+            if(length(target_gene_ind)>0)
+              simu_count[source_gene_ind,nearid] = fc*sim_cnt[source_gene_ind,nearid]
           }
+        }
       }
     }
   }
@@ -249,7 +166,7 @@ srtsim_cci_ref = function(
     metaParam = list(
       simSeed = sim_seed,
       simCCIKNN= numKNN,
-      simParam = list(background_param = newEstParam, effect_size = fc),
+      simParam = list(background_param = newEstParam),
       simType = "CCI_REF")
   )
   return(simsrt)
@@ -259,12 +176,103 @@ unlockBinding("srtsim_cci_ref", asNamespace("SRTsim"))
 assign("srtsim_cci_ref", srtsim_cci_ref, envir = asNamespace("SRTsim"))
 lockBinding("srtsim_cci_ref", asNamespace("SRTsim"))
 
-gp_df <- read_csv("./omnipath_lr_simulations.csv")
+##========================
+## Configuration
+##========================
+#setwd("/Users/sebastian.birk/Downloads")
+isid = 1
+#foldchange = 3 # foldchange, 1, 5, 10
+set.seed(isid)
+
+##========================
+## Load reference data
+##========================
+load(paste0("./starmap_1020_0410_seurat_filter_layer.rds"))
+info2   <- info %>% select(c(x,y,label)) %>% 
+				filter(label!="Smc") %>% as.data.frame()
+
+##========================
+## Define regions and cell type proportions
+##========================
+region_celltype_df <- matrix(0.1,4,4) # dim (regions, cell types)
+diag(region_celltype_df) <- 0.7
+rownames(region_celltype_df) <- paste0("Region",1:4)
+colnames(region_celltype_df) <- paste0("Celltype",1:4)
+
+##========================
+## Set simulation params
+##========================
+nGene = 20000 # unique sampled genes in prior GPs
+nLoc = 50000
+fc = "weak"
+
+##========================
+## Simulate spatial locations
+##========================
+simLoc <- simNewLocs(newN= nLoc,lay_out="random",preLoc = info2)
+
+##========================
+## Assign region labels
+##========================
+simLoc %<>% mutate(region_label = case_when(
+		x <= 0.5*median(x) ~ "Region1",
+		x > 0.5*median(x) & x <= median(x)~ "Region2",
+		x > median(x) & x <= 1.5*median(x)~ "Region3",
+		TRUE ~ "Region4"
+	)) %>% as.data.frame()
+
+##========================
+## Load ligand-receptor data
+##========================
+#load("./LR_dat.RData")
+
+##========================
+## Generate data w/o reference
+##========================
+#example_CCI_free = srtsim_cci_free(
+#					zero_prop_in = 0,
+#					disper_in = Inf,
+#					mu_in = 1, 
+#					numGene = nGene,
+#					location_in  = simLoc[,c("x","y","region_label")],
+#					# region_label = region_label,
+#					region_cell_map = region_celltype_df,
+#					sim_seed = isid,
+#					fc = foldchange,
+#					LR_in = LR_dat)
+
+##========================
+## Learn domain-specific (here domains are cell types) count distributions of reference data
+##========================
+simSRT  <- createSRT(count_in=sp_count[,rownames(info2)],loc_in =info2)
+simSRT1 <- srtsim_fit(simSRT,sim_schem="domain")
+
+##========================
+## Generate data with reference
+##========================
+#example_CCI_ref = srtsim_cci_ref(
+#					EstParam = simSRT1@EstParam,
+#					numGene = nGene,
+#					location_in  = simLoc[,c("x","y","region_label")],
+#					# region_label = region_label,
+#					region_cell_map = region_celltype_df,
+#					sim_seed = isid,
+#					fc = foldchange,
+#					LR_in = LR_dat)
+
+##========================
+## Read ligand-receptor-target data (including de-novo GPs)
+##========================
+file_path <- paste("./combined_denovo_gps_simulations_", fc, "_fc.csv", sep = "")
+gp_df <- read_csv(file_path)
 head(gp_df)
 
-data$sources <- lapply(data$sources, function(x) unlist(strsplit(x, ",")))
-data$targets <- lapply(data$targets, function(x) unlist(strsplit(x, ",")))
+gp_df$sources <- lapply(gp_df$sources, function(x) unlist(strsplit(x, ",")))
+gp_df$targets <- lapply(gp_df$targets, function(x) unlist(strsplit(x, ",")))
 
+##========================
+## Run reference-based simulation
+##========================
 example_CCI_ref = srtsim_cci_ref(
   EstParam = simSRT1@EstParam,
   numGene = nGene,
@@ -272,11 +280,15 @@ example_CCI_ref = srtsim_cci_ref(
   # region_label = region_label,
   region_cell_map = region_celltype_df,
   sim_seed = isid,
-  GP_in = gp_df)
+  GP_in = gp_df,
+  numSingleCellType = nLoc/5*2)
 
 gp_df$sources <- sapply(gp_df$sources, function(x) paste(x, collapse = ","))
 gp_df$targets <- sapply(gp_df$targets, function(x) paste(x, collapse = ","))
 
+##========================
+## Convert to h5ad object
+##========================
 sce_ref <- SingleCellExperiment(
   list(counts=example_CCI_ref@simCounts),
   colData=DataFrame(region_label=example_CCI_ref@simcolData$region_label,
@@ -287,4 +299,73 @@ sce_ref <- SingleCellExperiment(
 rownames(sce_ref) <- example_CCI_ref@simCounts@Dimnames[[1]]
 colnames(sce_ref) <- example_CCI_ref@simCounts@Dimnames[[2]]
 metadata(sce_ref)$gp_data <- gp_df
-writeH5AD(sce_ref, file = "./simulated_ref_omnipath.h5ad")
+save_path <- paste("./simulated_ref_", nGene, "genes_", nLoc, "locs_", fc, "fc.h5ad", sep = "")
+
+##========================
+## Run all reference-based simulations
+##========================
+isid = 1
+#foldchange = 3 # foldchange, 1, 5, 10
+set.seed(isid)
+
+for(fc in c("weak", "medium", "strong")){
+  for(nLoc in c(5000, 50000)){
+    for(nGene in c(500, 20000)){
+      load(paste0("./starmap_1020_0410_seurat_filter_layer.rds"))
+      info2   <- info %>% select(c(x,y,label)) %>% 
+        filter(label!="Smc") %>% as.data.frame()
+      
+      region_celltype_df <- matrix(0.1,4,4) # dim (regions, cell types)
+      diag(region_celltype_df) <- 0.7
+      rownames(region_celltype_df) <- paste0("Region",1:4)
+      colnames(region_celltype_df) <- paste0("Celltype",1:4)
+      
+      simLoc <- simNewLocs(newN= nLoc,lay_out="random",preLoc = info2)
+      simLoc %<>% mutate(region_label = case_when(
+        x <= 0.5*median(x) ~ "Region1",
+        x > 0.5*median(x) & x <= median(x)~ "Region2",
+        x > median(x) & x <= 1.5*median(x)~ "Region3",
+        TRUE ~ "Region4"
+      )) %>% as.data.frame()
+      
+      simSRT  <- createSRT(count_in=sp_count[,rownames(info2)],loc_in =info2)
+      simSRT1 <- srtsim_fit(simSRT,sim_schem="domain")
+      
+      file_path <- paste("./combined_denovo_gps_simulations_", fc, "_fc.csv", sep = "")
+      gp_df <- read_csv(file_path)
+      gp_df$sources <- lapply(gp_df$sources, function(x) unlist(strsplit(x, ",")))
+      gp_df$targets <- lapply(gp_df$targets, function(x) unlist(strsplit(x, ","))) 
+      
+      example_CCI_ref = srtsim_cci_ref(
+        EstParam = simSRT1@EstParam,
+        numGene = nGene,
+        location_in  = simLoc[,c("x","y","region_label")],
+        # region_label = region_label,
+        region_cell_map = region_celltype_df,
+        sim_seed = isid,
+        GP_in = gp_df,
+        numSingleCellType = nLoc/5*2
+        )
+    
+      gp_df$sources <- sapply(gp_df$sources, function(x) paste(x, collapse = ","))
+      gp_df$targets <- sapply(gp_df$targets, function(x) paste(x, collapse = ","))
+      
+      ##========================
+      ## Convert to h5ad object
+      ##========================
+      sce_ref <- SingleCellExperiment(
+        list(counts=example_CCI_ref@simCounts),
+        colData=DataFrame(region_label=example_CCI_ref@simcolData$region_label,
+                          cell_type=example_CCI_ref@simcolData$celltype,
+                          x=example_CCI_ref@simcolData$x,
+                          y=example_CCI_ref@simcolData$y))
+      
+      rownames(sce_ref) <- example_CCI_ref@simCounts@Dimnames[[1]]
+      colnames(sce_ref) <- example_CCI_ref@simCounts@Dimnames[[2]]
+      metadata(sce_ref)$gp_data <- gp_df
+      
+      save_path <- paste("./simulated_ref_", nGene, "genes_", nLoc, "locs_", fc, "fc.h5ad", sep = "")
+      writeH5AD(sce_ref, file = save_path)
+    }
+  }
+}
