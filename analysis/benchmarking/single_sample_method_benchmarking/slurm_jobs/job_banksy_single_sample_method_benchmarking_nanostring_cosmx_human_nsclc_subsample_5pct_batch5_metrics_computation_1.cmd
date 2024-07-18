3c80f1e200c89a24b794b78412c8ee8fe5b42260
sebastian.birk <sebastian.birk@outlook.com>
2024-07-18 08:48:21 +0200

    Finish reworking single sample method benchmarking

---
+++ analysis/benchmarking/single_sample_method_benchmarking/slurm_jobs/job_banksy_single_sample_method_benchmarking_nanostring_cosmx_human_nsclc_subsample_5pct_batch5_metrics_computation_1.cmd
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -J banksy_single_sample_method_benchmarking_nanostring_cosmx_human_nsclc_subsample_5pct_batch5_metrics_computation_1
+#SBATCH -o ./benchmarking/single_sample_method_benchmarking/slurm_jobs/logs/out_banksy_single_sample_method_benchmarking_nanostring_cosmx_human_nsclc_subsample_5pct_batch5_metrics_computation_1.txt
+#SBATCH -e ./benchmarking/single_sample_method_benchmarking/slurm_jobs/logs/err_banksy_single_sample_method_benchmarking_nanostring_cosmx_human_nsclc_subsample_5pct_batch5_metrics_computation_1.txt
+#SBATCH -t 48:00:00
+#SBATCH -p gpu_p
+#SBATCH -c 6
+#SBATCH --gres=gpu:1
+#SBATCH --qos=gpu_normal
+#SBATCH --mem=156G
+#SBATCH --nice=10000
+source $HOME/.bashrc
+conda activate nichecompass
+cd /
+cd /home/aih/sebastian.birk/workspace/projects/nichecompass-reproducibility/analysis/benchmarking/single_sample_method_benchmarking
+python ../compute_benchmarking_metrics.py --dataset nanostring_cosmx_human_nsclc_subsample_5pct_batch5 --task single_sample_method_benchmarking --file_name nanostring_cosmx_human_nsclc_subsample_5pct_batch5_banksy.h5ad --cell_type_key cell_type --batch_key None --latent_key banksy_latent --metrics gcs mlami cas clisis nasw cnmi cari casw clisi
