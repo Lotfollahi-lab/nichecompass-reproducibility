355a12c10fe189a536ed0ec13cd960c2a932059d
sebastian.birk <sebastian.birk@outlook.com>
2024-07-23 10:50:31 +0200

    Update single sample method benchmarking with review datasets and figures

---
+++ analysis/benchmarking/single_sample_method_benchmarking/slurm_jobs/job_staci_single_sample_method_benchmarking_sim1_1105genes_10000locs_strongincrements_metrics_computation_1.cmd
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -J staci_single_sample_method_benchmarking_sim1_1105genes_10000locs_strongincrements_metrics_computation_1
+#SBATCH -o ./benchmarking/single_sample_method_benchmarking/slurm_jobs/logs/out_staci_single_sample_method_benchmarking_sim1_1105genes_10000locs_strongincrements_metrics_computation_1.txt
+#SBATCH -e ./benchmarking/single_sample_method_benchmarking/slurm_jobs/logs/err_staci_single_sample_method_benchmarking_sim1_1105genes_10000locs_strongincrements_metrics_computation_1.txt
+#SBATCH -t 48:00:00
+#SBATCH -p gpu_p
+#SBATCH -c 6
+#SBATCH --gres=gpu:1
+#SBATCH --qos=gpu_normal
+#SBATCH --mem=156G
+#SBATCH --nice=10000
+source $HOME/.bashrc
+conda activate nichecompass
+cd /
+cd /home/aih/sebastian.birk/workspace/projects/nichecompass-reproducibility/analysis/benchmarking/single_sample_method_benchmarking
+python ../compute_benchmarking_metrics.py --dataset sim1_1105genes_10000locs_strongincrements --task single_sample_method_benchmarking --file_name sim1_1105genes_10000locs_strongincrements_staci.h5ad --cell_type_key cell_type --niche_type_key niche_type --batch_key None --latent_key staci_latent --metrics gcs mlami cas clisis nasw cnmi cari casw clisi --include_sdmbench
